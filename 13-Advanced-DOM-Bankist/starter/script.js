20ac0899bcbb666e12f49608fa3aebc0edc34442
Coralin <coralinpableo22@gmail.com>
2024-06-10 09:21:23 +0900

    Practice the OOP

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,31 +1,36 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+//Creating and inserting elements
+const header = document.querySelector('.header');
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent =
+  'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics.<button class="btn btn--close-cookie">Got it!</button';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+// header.prepend(message);
+header.append(message);
+// header.append(message).cloneNode(true);
+// header.before(message); add the cookies before the header
+// header.after(message);  add the cookies after the header
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+//!!!!Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+//styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,77 @@
-'use strict';
+// 'use strict';
+
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// const Jonas = new Person('Jonas', 1991);
+// const Cora = new Person('Coralin ', 1994);
+// console.log(Cora);
+
+// //1. New {} Empty Object is created
+// //2. functions is called, this  = {}
+// //3. Linked to a prototyped
+// //4.function automatically return {}
+// console.log(Person.prototype);
+// //Prototypes
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// Jonas.calcAge();
+// Jonas.calcAge();
+// console.log(Jonas.__proto__);
+// console.log(Jonas.__proto__ === Person.prototype);
+
+// console.log(Jonas.hasOwnProperty('firstName'));
+// //object.prototype (top of prototype chain)
+// console.log(Jonas.__proto__.__proto__);
+
+// console.dir(Person.prototype.constructor);
+
+// const arr = [3, 6, 6, 5, 5, 3, 6, 7, 5];
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// const result = arr.reduce((a, b) => {
+//   return a + b;
+// }, 0);
+
+// console.log(result);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+// console.log(arr.unique());
+
+class Plants {
+  constructor(name, kind, color) {
+    this._name = name;
+    this._kind = kind;
+    this._color = color;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get kind() {
+    return this._kind;
+  }
+
+  get color() {
+    return this._color;
+  }
+}
+
+let plants = [
+  new Plants('Rose', 'flower', 'red'),
+  new Plants('Cactos', 'flower', 'Green'),
+  new Plants('Tomato', 'Vegetable', 'red'),
+];
+console.log(
+  Plants.rose.forEach(srt => {
+    return `Hey ${srt}`;
+  })
+);
